4dfa4dd3b092740db5675e2ecae53911d47d9f55
James Perkins <jamesperkins@hey.com>
2021-07-02 14:09:48 -0400

    PUSH UP CODE

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,49 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import firebase from "firebase/app";
+import { config } from "../config/firebase.js";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+} else {
+  firebase.app();
 }
 
-export default MyApp
+const clerkFrontendApi = process.env.NEXT_PUBLIC_CLERK_FRONTEND_API;
+const clerkSignInURL = process.env.NEXT_PUBLIC_CLERK_SIGN_IN;
+
+const publicPages = [];
+
+function RedirectToSignIn() {
+  useEffect(() => {
+    window.location = clerkSignInURL;
+  });
+  return null;
+}
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  return (
+    <ClerkProvider
+      frontendApi={clerkFrontendApi}
+      navigate={(to) => router.push(to)}
+    >
+      {publicPages.includes(router.pathname) ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+        </>
+      )}
+    </ClerkProvider>
+  );
+}
+
+export default MyApp;
